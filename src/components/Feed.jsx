84ba331d8cf07ecdb69ab8ff9f93305c6914be0e
Sterling <sterlingduncan9@gmail.com>
2023-07-22 12:08:22 -0400

    added video cards and channel title and video title and next is connecting the ChannelId

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,18 +1,29 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import{ Sidebar, Videos } from "./";
+
 
 const Feed = () => {
+const [selectedCategory, setSelectedCategory] = useState('New');
+const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+   const data = fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
         sx={{
           height: { sx: "auto", md: "92vh" },
           borderRight: "1px solid #3d3d3d",
-          px: { sx: 0, md: 2 },
-        }}
-      >
-        <Sidebar />
+          px: { sx: 0, md: 2 },}}>
+        <Sidebar 
+          selectedCategory= {selectedCategory}
+          setSelectedCategory= {setSelectedCategory}
+        />
 
         <Typography
           className="copyright"
@@ -32,8 +43,10 @@ const Feed = () => {
             color: "white",
           }}
         >
-          New <span style={{ color: "#FC1503" }}>videos</span>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
